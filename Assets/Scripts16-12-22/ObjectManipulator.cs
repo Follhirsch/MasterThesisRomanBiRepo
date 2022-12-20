60ac66946715395ad5bd0518fa391f356405c0fc
Roman <romanbi@student.ethz.ch>
2022-12-20 17:14:52 +0100

    jaja

--- Assets/Scripts16-12-22/ObjectManipulator.cs
+++ Assets/Scripts16-12-22/ObjectManipulator.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObjectManipulator : MonoBehaviour
 {
+    public bool loadFromFile = false;
+    public TextAsset replayFile;
     public GameObject recorderSource;
 
     public GameObject sceneTarget;
@@ -23,7 +26,15 @@ public class ObjectManipulator : MonoBehaviour
         if (Input.GetKeyDown("1"))
         {
             //get hand posees
-            loadFromGame();
+            if(loadFromFile)
+            {
+                loadFromCSVFile();
+            }
+            else
+            {
+                loadFromGame();
+            }
+
         }
         if (Input.GetKeyDown("2"))
         {
@@ -41,6 +52,31 @@ public class ObjectManipulator : MonoBehaviour
         oriArray = recorderSource.GetComponent<ObjectRecorder>().oriQuaternion.ToArray();
 
     }
+    void loadFromCSVFile()
+    {
+        string[] dataLines = replayFile.text.Split("\n");
+        string[] header = dataLines[0].Split(",");
+        int frames = dataLines.Length;
+        int objects = header.Length/3;
+        
+        posArray = new Vector3[frames][objects];
+     
+        for (int i = 1; i < dataLines.Length/3; i++)
+        {
+            string[] dataValues = dataLines[i].Split(",");
+            for (int ii = 0; ii < dataValues.Length; ii= ii + 3)
+            {
+                Vector3 positionData = new Vector3(float.Parse(dataValues[ii]),float.Parse(dataValues[ii+1]),float.Parse(dataValues[ii+2]));
+                posArray[i][ii] = positionData;
+                //oriArray = 
+            }
+
+
+        }
+
+    }
+
+
     void playFrame()
     {
         if (posArray != null && oriArray != null)
@@ -77,6 +113,7 @@ public class ObjectManipulator : MonoBehaviour
                 Debug.Log("Positions not loaded");
             }
             yield return new WaitForSeconds(1 / recorderSource.GetComponent<ObjectRecorder>().samplingFrequency);
+            
         }
 
     }
